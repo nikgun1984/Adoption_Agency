16e1466e59983d7d13eb60456797f06f42046be4
Nick Gundobin <ngundobin@gmail.com>
2020-10-20 21:44:51 -0400

    more validations added for forms

--- forms.py
+++ forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField, BooleanField
-from wtforms.validators import InputRequired, Optional, Email
+from wtforms import StringField, IntegerField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import InputRequired, Optional, Email, NumberRange, URL
 
 class AddPetForm(FlaskForm):
 
     name = StringField("Pet's Name", validators=[InputRequired(message="Pet's Name cannot be blank")])
-    species = StringField("Species", validators=[InputRequired(message="Do not leave out this Species field blank")])
-    photo = StringField("Photo URL of Pet", validators=[Optional()])
-    age = IntegerField("Age of Pet",validators=[Optional()])
+    #species = StringField("Species", validators=[InputRequired(message="Do not leave out this Species field blank")])
+    species = SelectField('Species', choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')])
+    photo = StringField("Photo URL of Pet", validators=[Optional(),URL(require_tld=True, message="Should be a vaild URL")])
+    age = IntegerField("Age of Pet",validators=[NumberRange(min=0, max=30, message="Age should be between 0 to 30"),Optional()])
     notes = TextAreaField("Other Notes", validators=[Optional()])
     available = BooleanField("Is Available?")
